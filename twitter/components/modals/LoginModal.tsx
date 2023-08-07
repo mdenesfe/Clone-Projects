55105a2ca7acf9daef5fb3e82e6990d0c53ed5eb
Enes Demir <1200606034@nku.edu.tr>
2023-08-08 02:05:31 +0300

    Auth UI

---
+++ twitter/components/modals/LoginModal.tsx
@@ -0,0 +1,60 @@
+import useLoginModal from "@/hooks/useLoginModal";
+import { useCallback, useState } from 'react';
+import Input from "../Input";
+import Modal from "../Modal";
+
+const LoginModal = () => {
+    const loginModal = useLoginModal();
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+    const onSubmit = useCallback(async () => {
+        try {
+            setIsLoading(true)
+
+            // Login
+            
+            loginModal.onClose();
+
+        } catch (error) {
+            console.log(error); 
+        } finally {
+           setIsLoading(false);
+        }
+    }, [loginModal]);
+
+    const bodyContent = (
+        <div className="flex flex-col gap-4">
+            <Input 
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                disabled={isLoading}
+            />
+            <Input 
+                placeholder="Şifre"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                disabled={isLoading}
+            />
+
+        </div>
+
+    )
+
+    return (
+        <Modal
+            disabled={isLoading}
+            isOpen={loginModal.isOpen}
+            title="Giriş Yap"
+            actionLabel="Kayıt Ol"
+            onClose={loginModal.onClose}
+            onSubmit={onSubmit}
+            body={bodyContent}
+        />
+     );
+}
+ 
+export default LoginModal;
--- twitter/pages/_app.tsx
+++ twitter/pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
 import '@/styles/globals.css'
+import LoginModal from '@/components/modals/LoginModal'
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    <LoginModal/>
         <Layout>
           <Component {...pageProps} />
         </Layout>
